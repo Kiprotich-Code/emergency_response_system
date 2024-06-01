482b844a24dc047190091ae1e2c82d35337a91dd
Lawi Kiprotich <fleeklawi@gmail.com>
2024-06-01 21:01:25 +0300

    setting up admin dashboard

---
+++ administrator/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from accounts.models import CustomUser
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'full_names', 'user_type', 'location', 'password1', 'password2']
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'full_names', 'user_type', 'location', 'password']
---
+++ static/js/custom.js
@@ -0,0 +1,27 @@
+function toggle_sidenav() {
+    if ($('#sidenav').css('left') == '-250px') {
+        $('#sidenav').css('left', '0');
+        $('.can_have_sidenav').css('margin-left', '250px');
+    } else {
+        $('#sidenav').css('left', '-250px');
+        $('.can_have_sidenav').css('margin-left', '0');
+    }
+}
+
+
+$('.dropdown-btn').click(function () {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "none") {
+        dropdownContent.style.display = "block";
+    } else {
+        dropdownContent.style.display = "none";
+    }
+});
+
+setTimeout(function() {
+    let alert = document.getElementById('myAlert');
+    if (alert) {
+      alert.style.display = 'none';
+    }
+  }, 10000)

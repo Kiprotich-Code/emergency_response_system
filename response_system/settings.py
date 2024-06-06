71fac5eb238a0f8850c5d804d6220ac4bf2bd6c3
Lawi Kiprotich <fleeklawi@gmail.com>
2024-06-06 15:51:26 +0300

    .gitignore initial setup for hosting

--- response_system/settings.py
+++ response_system/settings.py
@@ -68,12 +68,19 @@ WSGI_APPLICATION = 'response_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# settings.py
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'PlWGAhxwDKnIaElZt',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '42197',
     }
 }
+
 
 
 # Password validation
